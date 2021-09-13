b66f296a0e82af83ce7a76e1b8902771a7724843
uGSecurity <52516035+uGSecurity@users.noreply.github.com>
2021-09-13 10:50:38 -0400

    Add files via upload

---
+++ CIS-150-Project 1.cpp
@@ -0,0 +1,12 @@
+//_____________________________________________________________
+//Author: Brandon Webb
+//Date : 9/2/2021
+//Desc : Simple Program to test Visual Studio. Hello World!
+//_____________________________________________________________
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello Mr Moore, I am excited to take this course! \n";
+}
+

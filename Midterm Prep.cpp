f84a07939ae168c02628740d39b67c726f00dcc9
uGSecurity <52516035+uGSecurity@users.noreply.github.com>
2021-10-14 14:26:41 -0400

    Add files via upload

--- Midterm Prep.cpp
+++ Midterm Prep.cpp
@@ -9,34 +9,69 @@ int age;
 
 int main()
 {
-    std::string name;
-    int pos;
+    std::string first;
+    std::string last;
+    std::string word;
     std::cout << "What year were you born: ";
     std::cin >> year;
     age = 2021 - year;
     std::cout << "Enter your first and last name: ";
-    std::cin >> name; "\n";
-    std::cout << "Hello" << " " << name << " " << "you are" << " " << age << " " "years old."; 
+    std::cin >> first >> last; "\n";
+    std::cout << "Hello" << " " << first << " " << last << " " << "you are" << " " << age << " " "years old.";
     std::cout << " ";
    
     if (age < 12) 
     {
       std::cout << "So you are a child.\n";
+      std::cout << "First letter of your first name: ";
+      std::cout << first.at(0);
+      std::cout << "\n";
+      std::cout << "Last letter of your last name: ";
+      std::cout << last.at(0);
     }
     else if (age > 12 && age < 17)
     {
-      std::cout << "So you ara a teenager.\n";
+      std::cout << "So you are a teenager.\n";
+      std::cout << "Enter a 3 letter word: ";
+      std::cin >> word;
+      if (word.length() == 3)
+      {
+          std::cout << "Original: " << word;
+          std::cout << "Backwards: " << word;
+          
+          
+      }
+      if (word.length() > 3)
+      {
+          std::cout << "You entered a word that is more than 3 letters.";
+      } 
+      if (word.length() < 3)
+      {
+          std::cout << "You entered a word that is less than 3 letters.";
+      }
+      std::cout << "\n";
     }
     else 
     {
       std::cout << "So you are an adult.\n";
+      std::cout << "First letter of your first name: ";
+      std::cout << first.at(0);
+      std::cout << "\n";
+      std::cout << "Last letter of your last name: ";
+      std::cout << last.at(0);
     }
     
    
-    std::cout << "First letter of your first name: ";
-    std::cout << name.at(0);
-    std::cout << "\n";
-    std::cout << "Last letter of your last name: \n";
+  
+
+
+
+
+
+
+
+
+
 }
 
 
